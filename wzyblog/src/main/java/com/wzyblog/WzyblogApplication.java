d946dac47ee538ea0c1ba5edfb37a0dba40f3cf2
wzy <852517791@qq.com>
2021-10-27 21:35:16 +0800

    项目框架搭建

---
+++ wzyblog/src/main/java/com/wzyblog/WzyblogApplication.java
@@ -0,0 +1,13 @@
+package com.wzyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzyblogApplication.class, args);
+    }
+
+}
---
+++ wzyblog/src/test/java/com/wzyblog/WzyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
